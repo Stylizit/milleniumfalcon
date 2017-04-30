97e68caa6bed999dbb327278872306b2c3dfd24b
Matt R <matthieu.rigolot@gmail.com>
2017-04-30 11:09:06 +0100

    working flight

--- public/js/main.js
+++ public/js/main.js
@@ -9,9 +9,19 @@ $(document).ready(function () {
 // http://demo.f4map.com/#lat=51.5288794&lon=-0.0839835&zoom=18&camera.theta=80&camera.phi=1.719
 
   var f4map;
-  var flight_number = "aca143";
+  var flight_number = "swr318";
+  var ANIMATION_TIME = 5000;
+  var lastPoint;
+  var counter = 0;
 
-  Number.prototype.toRadians = function() { return this * Math.PI / 180; };
+
+  function toRadians(n) { 
+    return n * Math.PI / 180;
+  }
+
+  function toDegrees(n) {
+    return (180 / Math.PI) * n;
+  }
 
   function destinationPoint(distance, bearing, radius) {
       radius = (radius === undefined) ? 6371e3 : Number(radius);
@@ -19,10 +29,10 @@ $(document).ready(function () {
       // see http://williams.best.vwh.net/avform.htm#LL
 
       var δ = Number(distance) / radius; // angular distance in radians
-      var θ = Number(bearing).toRadians();
+      var θ = toRadians(Number(bearing));
 
-      var φ1 = this.lat.toRadians();
-      var λ1 = this.lon.toRadians();
+      var φ1 = toRadians(lastPoint[1]);
+      var λ1 = toRadians(lastPoint[0]);
 
       var φ2 = Math.asin( Math.sin(φ1)*Math.cos(δ) +
                           Math.cos(φ1)*Math.sin(δ)*Math.cos(θ) );
@@ -30,11 +40,11 @@ $(document).ready(function () {
                                Math.cos(δ)-Math.sin(φ1)*Math.sin(φ2));
       λ2 = (λ2+3*Math.PI) % (2*Math.PI) - Math.PI; // normalise to -180..+180°
 
-      return new LatLon(φ2.toDegrees(), λ2.toDegrees());
+      return [toDegrees(λ2), toDegrees(φ2)];
   };
 
 
-var lastPoint;
+
   //init();
   createMap();
   
@@ -78,10 +88,19 @@ var lastPoint;
       queryData(flight_number, function(res) {
         var obj = res.data[Object.keys(res.data)[0]];
         console.log(obj);
+
+        var points = findEnRoute(obj.activityLog.flights).track;
+        var currentPoint = points[points.length - 1];
+        lastPoint = [currentPoint.coord[0], currentPoint.coord[1]];
+
+        console.log(lastPoint);
+
         var point = obj.activityLog.flights[0].origin.coord;
         var heading = obj.heading;
+        var speed = obj.groundspeed;
+        var distance = obj.distance;
 
-        center(point, heading);
+        center(point, heading, speed, distance);
       });
       
     }, 3000);
@@ -91,19 +110,21 @@ var lastPoint;
 
 
   function queryData(flight, cb) {
-    var api = 'http://10.0.1.125:3001/flightdata?flightCode=' + flight;
+    var api = 'http://localhost:3001/flightdata?flightCode=' + flight;
 
     $.get(api, function (data) {
       cb(data);
     });
   }
 
-  function center (point, heading) {
+  function center (point, heading, speed, distance) {
     window.f4map.panToBounds(new f4.map.LatLngBounds(f4.map.geometry.spherical.computeOffset([point[1], point[0]], 10, 200), new f4.map.geometry.spherical.computeOffset([point[1], point[0]], 100, 50)));
-    prepareFlight(heading);
+    prepareFlight(distance, speed, heading);
+
+    
   }
 
-  function prepareFlight(heading) {
+  function prepareFlight(distance, speed, heading) {
     setTimeout(function () {
       window.f4map._renderer.setHeading(heading);
       window.f4map._renderer.setTilt(80);
@@ -113,8 +134,9 @@ var lastPoint;
 
         //panToBounds(getFlightPoints()[1], getFlightPoints()[2]);
 
-        queryDataAtIntervals(flight_number);
+        //queryDataAtIntervals(flight_number);
 
+        animate(speed, heading, ANIMATION_TIME);
       }, 500);
     }, 3000);
   }
@@ -134,6 +156,35 @@ var lastPoint;
         console.log(url);
         changeSrc($iframe, url);
     });
+  }
+
+  function animate (speed, heading, time) {
+    var distance, cur;
+
+    setInterval(function () {
+      window.f4map._renderer.setHeading(heading + 180);
+      distance = speed * time / 5000;
+      console.log(distance);
+      console.log(speed);
+      cur = destinationPoint(distance, heading);
+
+      console.log(cur);
+      
+      
+      counter++;
+
+
+      if (true) {
+        panToBounds({
+          latitude: lastPoint[1],
+          longitude: lastPoint[0]
+        }, {
+          latitude: cur[1],
+          longitude: cur[0]
+        });
+        lastPoint = cur;
+      }
+    }, time);
   }
 
   function queryDataAtIntervals(flightname, time) {
